2b0274f5787db6e06cc2a0e14dbb7122d9ab14e0
Idealiza Urbanismo <idealizaurbanismo@Realiza2.local>
2017-12-13 14:57:07 -0200

    Quase pronto.

--- script.js
+++ script.js
@@ -4,24 +4,46 @@
         $("#inicial").hide(1000,"linear");
         $("#receitas").hide(1000,"linear");
         $("#nossosProdutos").hide(1000,"linear");
+     
+          $("#receitaArroz").hide(1000,"linear");
+             $("#receitaMocoto").hide(1000,"linear");
         $("#contatos").show(1000);
     });
  $("#menuInicial").click(function(){
         $("#contatos").hide(1000,"linear");
         $("#receitas").hide(1000,"linear");
+        $("#receitaArroz").hide(1000,"linear");
         $("#nossosProdutos").hide(1000,"linear");
+   
+           $("#receitaMocoto").hide(1000,"linear");
         $("#inicial").show(1000);
     });
 $("#menuProdutos").click(function(){
         $("#contatos").hide(1000,"linear");
         $("#receitas").hide(1000,"linear");
         $("#inicial").hide(1000,"linear");
+       
+          $("#receitaArroz").hide(1000,"linear");
+             $("#receitaMocoto").hide(1000,"linear");
         $("#nossosProdutos").show(1000);
     });
     $("#menuReceitas").click(function(){
         $("#contatos").hide(1000,"linear");
         $("#nossosProdutos").hide(1000,"linear");
         $("#inicial").hide(1000,"linear");
+      
+          $("#receitaArroz").hide(1000,"linear");
+          $("#receitaMocoto").hide(1000,"linear");
         $("#receitas").show(1000);
     });
+  
+     $("#btMocoto").click(function(){
+        $("#receitas").hide(1000,"linear");
+         $("#receitaMocoto").show(1000);
+    });
+    $("#btArroz").click(function(){
+        $("#receitas").hide(1000,"linear");
+         $("#receitaArroz").show(1000);
+    });
+     
 });
